a2a34f7acb0cfb74f53e2ff65da07c55521aa6a2
JCristinaJSalas <criistiinajerez@gmail.com>
2024-01-29 09:28:05 -0500

    feat: :sparkles: Create a Ninja Class

---
+++ Filtro_Julie/NarutoJava-Julie/src/main/java/com/mycompany/narutojava/julie/NarutoJavaJulie.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.narutojava.julie;
+
+/**
+ *
+ * @author user
+ */
+public class NarutoJavaJulie {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Filtro_Julie/NarutoJava-Julie/src/main/java/com/mycompany/narutojava/julie/ninja/model/clases/Ninja.java
@@ -0,0 +1,44 @@
+package com.mycompany.narutojava.julie.ninja.model.clases;
+
+public class Ninja {
+    private Long NinjaID;
+    private String nombre;
+    private String rango;
+    private String aldea;
+
+    public Long getNinjaID() {
+        return NinjaID;
+    }
+
+    public void setNinjaID(Long NinjaID) {
+        this.NinjaID = NinjaID;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getRango() {
+        return rango;
+    }
+
+    public void setRango(String rango) {
+        this.rango = rango;
+    }
+
+    public String getAldea() {
+        return aldea;
+    }
+
+    public void setAldea(String aldea) {
+        this.aldea = aldea;
+    }
+    
+    
+    
+}
+
